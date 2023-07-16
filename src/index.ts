5d059e18c0353347883affdbcf54cd5fb7aeac53
alanff97 <alan.figueroa.cl@gmail.com>
2023-07-16 18:35:00 +0200

    User model with get method

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-console.log("hi there")
+import { User } from './models/User';
+const userCreate = new User({ name: 'Alan', age: 26 });
+
+console.log(userCreate.get('name'));
+console.log(userCreate.get('age'));
