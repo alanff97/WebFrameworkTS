99f38a42b2c0bc28ad377f6125575dcbd90f91b8
alanff97 <alan.figueroa.cl@gmail.com>
2023-07-16 18:42:36 +0200

    set method in model Users

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { User } from './models/User';
 const userCreate = new User({ name: 'Alan', age: 26 });
+userCreate.set({ name: 'Santiago' });
 
 console.log(userCreate.get('name'));
 console.log(userCreate.get('age'));
