1dcf19ef79ab939804ba305cfdb73839ab71240a
alanff97 <alan.figueroa.cl@gmail.com>
2023-07-19 22:13:08 +0200

    method on for save events

--- src/models/User.ts
+++ src/models/User.ts
@@ -2,7 +2,9 @@ interface UserProps {
   name?: string;
   age?: number;
 }
+type Callback = () => void;
 export class User {
+  events: { [key: string]: Callback[] } = {};
   constructor(private data: UserProps) {}
   get(propName: string): number | string {
     return this.data[propName];
@@ -10,5 +12,10 @@ export class User {
 
   set(update: UserProps): void {
     Object.assign(this.data, update);
+  }
+  on(event: string, callback: Callback): void {
+    const handlers = this.events[event] || []; //Callback[] or undefined
+    handlers.push(callback);
+    this.events[event] = handlers;
   }
 }
