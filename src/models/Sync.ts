a331e911bca55322bb104c6ea33f454f18ca18ca
alanff97 <alan.figueroa.cl@gmail.com>
2023-09-07 10:21:34 +0200

    changes in Sync and User, extracting properties from user and separate in Sync

--- src/models/Sync.ts
+++ src/models/Sync.ts
@@ -1,21 +1,20 @@
 import axios, { AxiosPromise } from 'axios';
-import { UserProps } from './User';
-export class Sync {
+
+interface HasId {
+  id?: number;
+}
+export class Sync<T extends HasId> {
   constructor(public rootUrl: string) {} // new Sync(http://localhost:3000/users)
   fetch(id: number): AxiosPromise {
     return axios.get(`${this.rootUrl}/${id}`);
   }
 
-  save(data: UserProps): AxiosPromise {
-    try {
-      const { id } = data;
-      if (id) {
-        return axios.put(`${this.rootUrl}/${id}`, data);
-      } else {
-        return axios.post(this.rootUrl, data);
-      }
-    } catch (error) {
-      return error;
+  save(data: T): AxiosPromise {
+    const { id } = data;
+    if (id) {
+      return axios.put(`${this.rootUrl}/${id}`, data);
+    } else {
+      return axios.post(this.rootUrl, data);
     }
   }
 }
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,22 +1,18 @@
 
 import { Eventing } from './Eventing';
+import { Sync } from './Sync';
+
+
 export interface UserProps {
   name?: string;
   age?: number;
   id?: number;
 }
-
+const rootUrl= "http://localhost:3000/users"
 export class User {
   public events: Eventing = new Eventing();
-  constructor(private data: UserProps) {}
-  get(propName: string): number | string {
-    return this.data[propName];
-  }
-
-  set(update: UserProps): void {
-    // using the sign ? in  the interface because of this, we can update one or more properties
-    Object.assign(this.data, update);
-  }
+  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl)
+  
 
   
  
