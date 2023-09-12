8197008fbec918f1ed307484c63979a0dfc0fa9f
alanff97 <alan.figueroa.cl@gmail.com>
2023-09-12 13:35:54 +0200

    changed functions methods to arrow functions because the context of execution

--- src/models/Attributes.ts
+++ src/models/Attributes.ts
@@ -1,9 +1,9 @@
 export class Attributes<T extends object> {
   constructor(private data: T) {}
-  get<K extends keyof T>(key: K): T[K] {
+  get = <K extends keyof T>(key: K): T[K] => {
     // the only values of K its the keys of T, in this case UserProps(name,age,id)
     return this.data[key];
-  }
+  };
 
   set(update: T): void {
     // using the sign ? in  the interface because of this, we can update one or more properties
--- src/models/Eventing.ts
+++ src/models/Eventing.ts
@@ -2,13 +2,13 @@ type Callback = () => void;
 
 export class Eventing {
   events: { [key: string]: Callback[] } = {};
-  on(event: string, callback: Callback): void {
+  on = (event: string, callback: Callback): void => {
     const handlers = this.events[event] || []; //Callback[] or undefined
     handlers.push(callback);
     this.events[event] = handlers;
-  }
+  };
 
-  trigger(event: string): void {
+  trigger = (event: string): void => {
     const handlers = this.events[event];
     if (!handlers || handlers.length === 0) {
       return;
@@ -16,5 +16,5 @@ export class Eventing {
     handlers.forEach((callback) => {
       callback();
     });
-  }
+  };
 }
