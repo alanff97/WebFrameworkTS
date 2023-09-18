127888a52c058a9dd9f6ee4705b7a2d0f2f632f9
alanff97 <alan.figueroa.cl@gmail.com>
2023-09-18 15:56:34 +0200

    final refactor of User model

--- src/models/ApiSync.ts
+++ src/models/ApiSync.ts
@@ -3,7 +3,7 @@ import axios, { AxiosPromise } from 'axios';
 interface HasId {
   id?: number;
 }
-export class Sync<T extends HasId> {
+export class ApiSync<T extends HasId> {
   constructor(public rootUrl: string) {} // new Sync(http://localhost:3000/users)
   fetch(id: number): AxiosPromise {
     return axios.get(`${this.rootUrl}/${id}`);
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,8 +1,19 @@
 import { Model } from './Model';
+import { Attributes } from './Attributes';
+import { ApiSync } from './ApiSync';
+import { Eventing } from './Eventing';
 export interface UserProps {
   name?: string;
   age?: number;
   id?: number;
 }
 const rootUrl = 'http://localhost:3000/users';
-export class User {}
+export class User extends Model<UserProps> {
+  static buildUser(attrs: UserProps): User {
+    return new User(
+      new Attributes<UserProps>(attrs),
+      new Eventing(),
+      new ApiSync<UserProps>(rootUrl)
+    );
+  }
+}
